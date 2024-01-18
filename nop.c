e059c61942f3acd1795c777087bfaccebf00ded6
GiftMathotsi <giftnyiko234@gmail.com>
2024-01-18 09:54:13 -0800

    swap.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+ #include "monty.h"
+/**
+ * f_nop- nothing
+ * @head: stack head
+ * @counter: line number
+ * ReturnL no return 
+ */
+void f_nop(stack_t **head, unsigned in counter)
+{
+	(void) counter;
+	(void) head;
+}
